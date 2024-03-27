2ac5298af2f233b1c4a5888795bec0a6af236867
Yash Kunte <yash.kunte123@gmail.com>
2024-03-27 13:09:22 +0530

    adding db connection code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+// require('dotenv').config({path:'./env'})
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB();
+
+// import { express } from "express";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// const app = express();
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (error) => {
+//             console.log("Error: ",error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT, ()=> {
+//             console.log('app is listening on port: ', process.env.PORT)
+//         })
+//     }catch(error) {
+//         console.error("Error:", error);
+//         throw error
+//     }
+// })()
