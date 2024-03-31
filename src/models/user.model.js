036b0dbc11f3f52c6b33bb4de410513c09b0afc4
Yash Kunte <yash.kunte123@gmail.com>
2024-03-31 19:20:56 +0530

    added user and video models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -88,4 +88,4 @@ userSchema.methods.generateRefreshToken = function () {
   );
 };
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model('User', userSchema);
