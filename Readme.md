f6e731ef54d27aed04343fb8a244c98ccfa17873
Yash Kunte <yash.kunte123@gmail.com>
2024-04-04 12:03:32 +0530

    adding first routes and controller

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 # Backend project
+
+## Utils/asyncHandler.js :
+
+this asyncHandler function simplifies error handling for asynchronous route handlers in Express.js applications by ensuring that any errors thrown by the asynchronous operations are properly caught and passed along to Express's error handling middleware.
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,17 @@
+//  The purpose of asyncHandler is to wrap asynchronous route handlers in Express.js
+// applications, ensuring proper error handling for asynchronous operations.
+
 const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-    }
-}
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-
-export { asyncHandler }
-
-
-
+export { asyncHandler };
 
 // const asyncHandler = () => {}
 // const asyncHandler = (func) => () => {}
 // const asyncHandler = (func) => async () => {}
-
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
